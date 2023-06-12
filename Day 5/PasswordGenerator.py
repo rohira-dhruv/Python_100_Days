454987eed3e7bac7f722c42d3acbf00658401398
rohira-dhruv <rohirajdhruv@gmail.com>
2023-06-12 20:16:52 +0530

    complete day 5 and 6

---
+++ Day 5/PasswordGenerator.py
@@ -0,0 +1,28 @@
+# Random Password Generator
+
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the Password Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_numbers = int(input("How many numbers would you like?\n"))
+nr_symbols = int(input("How many symbols would you like?\n"))
+
+password = ""
+
+for i in range(0, nr_letters):
+    password = password + letters[random.randint(0, len(letters)-1)]
+
+for i in range(0, nr_numbers):
+    password = password + numbers[random.randint(0, len(numbers)-1)]
+
+for i in range(0, nr_symbols):
+    password = password + symbols[random.randint(0, len(symbols)-1)]
+
+password_list = list(password)
+random.shuffle(password_list)
+password = "".join(password_list)
+
+print(password)
