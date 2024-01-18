feda9a73b53d5b2bbc98df7b09b088da36d8a694
rohira-dhruv <dhruvjrohira@gmail.com>
2024-01-18 20:54:15 +0530

    Improved code readability

--- Intermediate/Day 18 - Hirst Painting/main.py
+++ Intermediate/Day 18 - Hirst Painting/main.py
@@ -4,6 +4,7 @@ import turtle
 import colorgram
 from turtle import Turtle, Screen
 
+# colorgram extracts a list of rgb tuple colors from a given image.
 colors = colorgram.extract("hirst.jpg", 30)
 rgb_colors = []
 for color in colors:
@@ -13,18 +14,20 @@ for color in colors:
     rgb_colors.append((r, g, b))
 
 color_list = [(140, 176, 207), (25, 32, 48), (26, 107, 159), (237, 225, 235), (209, 161, 111), (144, 29, 63),
-              (230, 212, 93), (4, 163, 197), (218, 60, 84), (229, 79, 43), (195, 130, 169), (54, 168, 114),
-              (28, 61, 116), (172, 53, 95), (108, 182, 90), (110, 99, 87), (193, 187, 46), (240, 204, 2), (1, 102, 119),
-              (19, 22, 21), (50, 150, 109), (172, 212, 172), (118, 36, 34), (221, 173, 188), (227, 174, 166),
-              (153, 205, 220), (184, 185, 210)]
+            (230, 212, 93), (4, 163, 197), (218, 60, 84), (229, 79, 43), (195, 130, 169), (54, 168, 114),
+            (28, 61, 116), (172, 53, 95), (108, 182, 90), (110, 99, 87), (193, 187, 46), (240, 204, 2), (1, 102, 119),
+            (19, 22, 21), (50, 150, 109), (172, 212, 172), (118, 36, 34), (221, 173, 188), (227, 174, 166),
+            (153, 205, 220), (184, 185, 210)]
 
 
 def set_random_color():
-    new_color = random.choice(color_list)
+    """Sets the color of the turtle object to a randomly selected color extracted from the image."""
+    new_color = random.choice(rgb_colors)
     timmy.color(new_color)
 
 
 def draw_dots():
+    """draws 10 dots horizontally on the screen."""
     for _ in range(10):
         timmy.pendown()
         set_random_color()
